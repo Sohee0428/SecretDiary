90db4924acdb31b1f34a4f526bf078e88a261c76
Sohee <sohee3480@gmail.com>
2021-08-13 22:44:36 +0900

    xml 구성요소 변수로 만들기

--- app/src/main/java/com/example/aop_part2_chapter03/MainActivity.kt
+++ app/src/main/java/com/example/aop_part2_chapter03/MainActivity.kt
@@ -1,11 +1,57 @@
 package com.example.aop_part2_chapter03
 
+import android.content.Context
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.NumberPicker
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.widget.AppCompatButton
 
 class MainActivity : AppCompatActivity() {
+
+    private val numberPicker1: NumberPicker by lazy {
+        findViewById<NumberPicker>(R.id.numberPicker1)
+            .apply {
+                minValue = 0
+                maxValue = 9
+            }
+    }
+
+    private val numberPicker2: NumberPicker by lazy {
+        findViewById<NumberPicker>(R.id.numberPicker2)
+            .apply {
+                minValue = 0
+                maxValue = 9
+            }
+    }
+
+    private val numberPicker3: NumberPicker by lazy {
+        findViewById<NumberPicker>(R.id.numberPicker3)
+            .apply {
+                minValue = 0
+                maxValue = 9
+            }
+    }
+
+    private val openBtn: AppCompatButton by lazy {
+        findViewById<AppCompatButton>(R.id.openBtn)
+    }
+
+    private val changePasswordBtn: AppCompatButton by lazy {
+        findViewById<AppCompatButton>(R.id.changePasswordBtn)
+    }
+
+    private var changePasswordMode = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        numberPicker1
+        numberPicker2
+        numberPicker3
+
     }
 }
