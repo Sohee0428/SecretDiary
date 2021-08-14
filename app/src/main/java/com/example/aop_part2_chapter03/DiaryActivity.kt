7c1f01e790a21b6b6e396259e55d441cd116562c
Sohee <sohee3480@gmail.com>
2021-08-14 23:29:23 +0900

    다이어리로 들어와서 작성한 글을 저장  - sharedPreferences 사용

--- app/src/main/java/com/example/aop_part2_chapter03/DiaryActivity.kt
+++ app/src/main/java/com/example/aop_part2_chapter03/DiaryActivity.kt
@@ -1,13 +1,19 @@
 package com.example.aop_part2_chapter03
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 
 class DiaryActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dialy)
 
+        val detailPreferences = getSharedPreferences("diary", Context.MODE_PRIVATE)
+        val diaryEdt = findViewById<EditText>(R.id.diaryEdt)
 
+        diaryEdt.setText(detailPreferences.getString("detail",""))
     }
 }
